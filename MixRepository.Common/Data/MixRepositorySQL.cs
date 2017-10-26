84db3551b48621abee60348013fb21901a0a7e1a
lemm0038 <jeff.lemmerman@gmail.com>
2017-10-26 15:59:29 -0500

    Added MixRepositorySQL

---
+++ MixRepository.Common/Data/MixRepositorySQL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MixRepository.Common.Model;
+
+namespace MixRepository.Common.Data
+{
+	public class MixRepositorySQL : IMixRepository
+	{
+		public void Add(IEnumerable<Mix> mixes)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int mixId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void GetById(int mixId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Mix mix)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
